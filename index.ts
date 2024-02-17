21f8b07f53b584ab340d9a2bcbe42d319ff8cb29
orzarhi <or.zarhi11@gmail.com>
2024-02-18 00:04:07 +0200

    Refactor promise handling in index.ts

--- index.ts
+++ index.ts
@@ -1,11 +1,6 @@
 /* Promise.race - first promise to resolve or reject will be returned */
-const promise1 = new Promise((resolve, reject) => {
-  setTimeout(resolve, 500, "one");
-});
-
-const promise2 = new Promise((resolve, reject) => {
-  setTimeout(resolve, 50, "two");
-});
+const promise1 = new Promise((resolve, reject) => setTimeout(resolve, 500, "one"));
+const promise2 = new Promise((resolve, reject) => setTimeout(reject, 50, "two"));
 
 Promise.race([promise1, promise2])
   .then((value) => {
@@ -18,42 +13,44 @@ Promise.race([promise1, promise2])
 // ------------------------------------------------
 
 /* Promise.any - first promise to resolve will be returned */
-const promise1 = Promise.reject(0);
-const promise2 = new Promise((resolve) => setTimeout(resolve, 1000, "quick"));
-const promise3 = new Promise((resolve) => setTimeout(resolve, 500, "slow"));
+// const promise1 = Promise.reject(0);
+// const promise2 = new Promise((resolve) => setTimeout(resolve, 100, "quick"));
+// const promise3 = new Promise((resolve) => setTimeout(resolve, 500, "slow"));
 
-const promises = [promise1, promise2, promise3];
+// const promises = [promise1, promise2, promise3];
 
-Promise.any(promises).then((value) => console.log(value));
+// Promise.any(promises)
+//   .then((value) => console.log(value))
+//   .catch((reason) => console.error(reason));
 
 // ------------------------------------------------
 
 /* Promise.allSettled - is typically used when you have multiple asynchronous tasks that are not dependent
  *  on one another to complete successfully, or you'd always like to know the result of each promise.
  */
-const promise1 = Promise.reject(3);
-const promise2 = new Promise((resolve, reject) =>
-  setTimeout(reject, 100, "seconde promise failed")
-);
-const promise3 = new Promise((resolve, reject) =>
-  setTimeout(reject, 1000, "third promise succeeded")
-);
-const promises = [promise1, promise2, promise3];
+// const promise1 = Promise.reject(3);
+// const promise2 = new Promise((resolve, reject) =>
+//   setTimeout(reject, 100, "seconde promise failed")
+// );
+// const promise3 = new Promise((resolve, reject) =>
+//   setTimeout(reject, 1000, "third promise succeeded")
+// );
+// const promises = [promise1, promise2, promise3];
 
-Promise.allSettled(promises).then((results) =>
-  results.forEach((result: any) => console.log(result.reason))
-);
+// Promise.allSettled(promises).then((results) =>
+//   results.forEach((result: any) => console.log(result.reason))
+// );
 
 // ------------------------------------------------
 
-/* Promise.all - all promises must resolve if not, returns the first rejection */
-const promise1 = Promise.resolve(3);
-const promise2 = 42;
-const promise3 = new Promise((resolve, reject) => {
-  setTimeout(resolve, 100, "third promise succeeded");
-});
-const promises = [promise1, promise2, promise3];
+/* Promise.all - all promises must resolve if not returns the first rejection */
+// const promise1 = Promise.resolve(3);
+// const promise2 = 42;
+// const promise3 = new Promise((resolve, reject) => {
+//   setTimeout(resolve, 100, "third promise succeeded");
+// });
+// const promises = [promise1, promise2, promise3];
 
-Promise.all(promises)
-  .then((values) => console.log(values))
-  .catch((reason) => console.error(reason));
+// Promise.all(promises)
+//   .then((values) => console.log(values))
+//   .catch((reason) => console.error(reason));
